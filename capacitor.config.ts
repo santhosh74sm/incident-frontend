e5a6559285733b0e3476957c9e4194ba9c836b25
santhosh74sm <santhosh74sm@gmail.com>
2026-06-01 16:05:20 +0530

    Android APP Try

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.santhosh.incidenttracker',
+  appName: 'Incident Tracking System',
+  webDir: 'build'
+};
+
+export default config;
