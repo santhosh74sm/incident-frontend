67cc7f5e6f65a80ed74397662464ee13a2b26cdc
santhosh74sm <santhosh74sm@gmail.com>
2026-06-02 13:20:28 +0530

    Add app icon and splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.santhosh.incidenttracker',
   appName: 'Incident Tracking System',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2500,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidScaleType: 'CENTER_INSIDE',
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
